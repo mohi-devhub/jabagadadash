04b631eec0c84456275f4cece086570ec69ab986
mohi-devhub <mohith.cvv230152@cvv.ac.in>
2026-02-02 23:48:37 +0530

    Add /events route with basic layout

---
+++ app/events/page.tsx
@@ -0,0 +1,7 @@
+export default function EventsPage() {
+  return (
+    <div className="min-h-screen bg-black p-8">
+      <h1 className="text-white text-4xl font-bold">Events</h1>
+    </div>
+  );
+}
